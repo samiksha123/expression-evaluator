af80e8416354a3560324bcc986c74c3775d2454e
samiksha <samiksha@thoughtworks.com>
2014-01-23 17:05:10 +0530

    expressions are working for floating numbers

--- src/library/Evaluator.java
+++ src/library/Evaluator.java
@@ -5,64 +5,66 @@ import java.util.List;
 
 public class Evaluator {
 
-    private int evaluate(int num1, String operator, int num2) {
-        int result = 0;
+    private double evaluate(double num1, String operator, double num2) {
+        double result = 0.0;
         if (operator.equals("+")) result = num1 + num2;
         if (operator.equals("-")) result = num1 - num2;
         if (operator.equals("*")) result = num1 * num2;
         if (operator.equals("/")) result = num1 / num2;
-        if (operator.equals("^")) result = (int) Math.pow(num1, num2);
+        if (operator.equals("^")) result = (float) Math.pow(num1, num2);
         return result;
     }
-    public int evaluateExpression(String expression) throws Exception {
-        if (!expression.contains(" ")) return Integer.parseInt(expression);
-        if (expression.contains("(")){
+
+    public double evaluateExpression(String expression) throws Exception {
+        if (!expression.contains(" ")) return Double.parseDouble(expression);
+        if (expression.contains("(")) {
             String res = evaluateWithBrackets(expression);
             return evaluateExpression(res);
         }
         String[] data = expression.split(" ");
-        List<Integer> operands = new ArrayList<Integer>();
+        List<Double> operands = new ArrayList<Double>();
         List<String> operators = getOperators(data, operands);
-        int result = evaluateMultipleOperations(operands, operators);
+        double result = evaluateMultipleOperations(operands, operators);
         return result;
     }
 
     private String evaluateWithBrackets(String expression) throws Exception {
         StringBuffer sb = new StringBuffer(expression);
-        int startIndex = 0,endIndex = 0;
-        for (int i = 0; i < expression.length() ; i++) {
-            if(expression.charAt(i) == '(')
-                startIndex = i;
-            else if(expression.charAt(i) == ')'){
+        int startIndex = 0, endIndex = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(') startIndex = i;
+            else if (expression.charAt(i) == ')') {
                 endIndex = i;
-                 break;
+                break;
             }
         }
         String expressionInBrackets = expression.substring(startIndex + 1, endIndex);
-        int res = evaluateExpression(expressionInBrackets);
-        sb.replace(startIndex, endIndex + 1, Integer.toString(res));
+        double res = evaluateExpression(expressionInBrackets);
+        sb.replace(startIndex, endIndex + 1, Double.toString(res));
         return sb.toString();
     }
-    private int evaluateMultipleOperations(List<Integer> operands, List<String> operators) throws Exception {
+
+    private double evaluateMultipleOperations(List<Double> operands, List<String> operators) throws Exception {
         Evaluator e = new Evaluator();
-        int result = e.evaluate(operands.get(0), operators.get(0), operands.get(1));
+        double result = e.evaluate(operands.get(0), operators.get(0), operands.get(1));
         for (int i = 1; i < operators.size(); i++) {
             result = e.evaluate(result, operators.get(i), operands.get(i + 1));
         }
         return result;
     }
-    private List<String> getOperators(String[] data, List<Integer> operands) {
+
+    private List<String> getOperators(String[] data, List<Double> operands) {
         List<String> operators = new ArrayList<String>();
         List list = new ArrayList();
         for (String s : data) {
             try {
-                int number = Integer.parseInt(s);
+                Double number = Double.parseDouble(s);
                 operands.add(number);
             } catch (Exception ex) {
                 operators.add(s);
             }
         }
-        if(operands.size()-1 == operators.size())
+        if (operands.size() - 1 == operators.size())
             return operators;
         list.add("wrong input");
         return list;
--- test/library/EvaluatorTest.java
+++ test/library/EvaluatorTest.java
@@ -1,97 +1,114 @@
 package library;
 
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
+import static junit.framework.Assert.assertEquals;
 
 public class EvaluatorTest extends Evaluator {
     @org.junit.Test
     public void testEvaluate() throws Exception {
         String expression = "2 + 3";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,5);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,5.0);
     }
     @org.junit.Test
     public void test_for_adding_two_numbers() throws Exception {
         String expression = "45 + 10";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,55);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,55.0);
     }
     @org.junit.Test
     public void test_if_only_one_operand_is_given() throws Exception {
         String expression = "22";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,22);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,22.0);
     }
     @org.junit.Test
     public void test_for_subtraction_of_two_numbers() throws Exception {
         String expression = "40 - 10";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,30);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,30.0);
     }
     @org.junit.Test
     public void test_for_multiplication_of_two_numbers() throws Exception {
         String expression = "4 * 10";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,40);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,40.0);
     }
     @org.junit.Test
     public void test_for_division_of_two_numbers() throws Exception {
         String expression = "22 / 3";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,7);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,7.0);
     }
     @org.junit.Test
     public void test_for_calculating_power_of_two_numbers() throws Exception {
         String expression = "2 ^ 3";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,8);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,8.0);
     }
     @org.junit.Test
     public void test_for_evaluating_multiple_operations() throws Exception {
         String expression = "1 + 3 * 2";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,8);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,8.0);
     }
     @org.junit.Test
     public void test_for_evaluate_multiple_operations() throws Exception {
         String expression = "4 + 5 * 2 / 3 ^ 2 - 3";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,33);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,33.0);
     }
     @org.junit.Test
     public void test_for_evaluate_multiple_operations_with_brackets() throws Exception {
         String expression = "4 + (5 * 2) / 2";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,7);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,7.0);
     }
     @org.junit.Test
     public void test_for_evaluate_multiple_operations_with_only_brackets() throws Exception {
         String expression = "(5 * 2 - 5 ^ 2)";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,25);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,25.0);
     }
     @org.junit.Test
     public void test_for_evaluate_multiple_operations_with_multiple_brackets() throws Exception {
         String expression = "4 + (5 * 2) / 2 - (2 / 2)";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,6);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,6.0);
     }
     @org.junit.Test
     public void test_for_evaluate_multiple_operations_with_brackets_inside_brackets() throws Exception {
         String expression = "4 + (5 * 2 + (3 + 1) - 2)";
         Evaluator e = new Evaluator();
-        int res = e.evaluateExpression(expression);
-        assertEquals(res,16);
+        double res = e.evaluateExpression(expression);
+        assertEquals(res,16.0);
+    }
+    @org.junit.Test
+    public void test_for_evaluate_multiple_operations_of_float() throws Exception {
+        String expression = "4.20 + 3.50";
+        Evaluator e = new Evaluator();
+        double res = e.evaluateExpression(expression);
+        double expected = 7.70;
+        assertEquals(res,expected);
+    }
+    @org.junit.Test
+    public void test_for_evaluate_multiple_operations_for_negative() throws Exception {
+        String expression = "4.20 + 3.50";
+        Evaluator e = new Evaluator();
+        double res = e.evaluateExpression(expression);
+        double expected = 7.70;
+        assertEquals(res,expected);
     }
 }
