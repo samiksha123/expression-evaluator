5e98579fe597e178ff6c21add225d5732da691c9
samiksha <samiksha@thoughtworks.com>
2014-02-07 19:35:34 +0530

    Extract Parser from expression

--- src/client/EvaluatorClient.java
+++ src/client/EvaluatorClient.java
@@ -1,10 +1,11 @@
 package client;
 
 import library.evaluation.Expression;
+import library.evaluation.Parser;
 
 public class EvaluatorClient {
     public static void main(String[] args) throws Exception {
         Expression e = new Expression();
-        System.out.println(e.parse(args[0]));
+        System.out.println(new Parser().parse(args[0]).getValue());
     }
 }
--- src/library/evaluation/Expression.java
+++ src/library/evaluation/Expression.java
@@ -20,42 +20,7 @@ public class Expression {
         this.operators = operators;
     }
 
-    public Expression parse(String expr) throws Exception {
-        String cleanedExpression = clean(expr);
-
-        if (cleanedExpression.contains("(")) {
-            String res = evaluateWithBrackets(cleanedExpression);
-            return new Expression().parse(res);
-        }
-
-        if (!cleanedExpression.contains(" "))
-            return new Expression(Double.parseDouble(cleanedExpression));
-
-        String[] data = cleanedExpression.split(" ");
-        return new Expression(getOperands(data), getOperators(data)).evaluate();
-    }
-
-
-    private String clean(String expr) {
-        return new Parser(expr).replaceExpression(expr).replace("--", "");
-    }
-
-    private String evaluateWithBrackets(String expression) throws Exception {
-        StringBuffer sb = new StringBuffer(expression);
-        int startIndex = 0, endIndex = 0;
-        for (int i = 0; i < expression.length(); i++) {
-            if (expression.charAt(i) == '(') startIndex = i;
-            else if (expression.charAt(i) == ')') {
-                endIndex = i;
-                break;
-            }
-        }
-        String expressionInBrackets = expression.substring(startIndex + 1, endIndex);
-        sb.replace(startIndex, endIndex + 1, String.valueOf(parse(expressionInBrackets).value));
-        return sb.toString();
-    }
-
-    private Expression evaluate(){
+    public Expression evaluate(){
         Operators calculator = new Operators();
         double result = calculator.operate(expressions.get(0).getValue(),
                 operators.get(0), expressions.get(1).getValue());
@@ -67,32 +32,9 @@ public class Expression {
         return this;
     }
 
-    private List<String> getOperators(String[] data) {
-        List<String> operators = new ArrayList<String>();
-        for (String s : data) {
-            try {
-                Double.parseDouble(s);
-            } catch (Exception ex) {
-                operators.add(s);
-            }
-        }
-        return operators;
-    }
-
-    private List<Expression> getOperands(String[] data) {
-        List<Expression> expressions = new ArrayList<>();
-        for (String s : data) {
-            try {
-                double v = Double.parseDouble(s);
-                expressions.add(new Expression(v));
-            } catch (Exception ex) {
-                // ignore
-            }
-        }
-        return expressions;
-    }
-
     public Double getValue() {
         return (value!=null)? value: this.evaluate().getValue() ;
     }
+
+
 }
